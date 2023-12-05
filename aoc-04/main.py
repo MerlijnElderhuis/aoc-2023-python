6c3cacfa9b379d42a8fbb1a27bacb5d62313457d
Merlijn <merlijn@brainial.com>
2023-12-05 19:51:38 +0100

    day 4 pt 2

--- aoc-04/main.py
+++ aoc-04/main.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import os
 
 
@@ -32,7 +33,7 @@ def lines_to_cards(lines):
 
 
 def main():
-    TEST = True
+    TEST = False
     if TEST:
         file_name = "test_input.txt"
     else:
@@ -45,13 +46,15 @@ def main():
     print("=" * 80)
 
     all_cards = lines_to_cards(lines)
+    all_cards_dict = {i: card for i, card in enumerate(all_cards)}
+
     for card in all_cards:
         print(card)
 
     print("=" * 80)
 
     ### Pt1
-    pts = 0
+    pts_1 = 0
     for i, card in enumerate(all_cards):
         card_intersect = set(card[0]) & set(card[1])
         if not card_intersect:
@@ -60,7 +63,39 @@ def main():
             print(f"Found intersect on Card {i}: {len(card_intersect)}")
             pts_card = pow(2, (len(card_intersect) - 1))
             print(f"adding pts: {pts_card}")
-            pts += pts_card
+            pts_1 += pts_card
+
+    def calc_card_res(card_nr: int) -> int:
+        card = all_cards_dict[card_nr]
+        card_intersect = set(card[0]) & set(card[1])
+        return len(card_intersect)
+
+    all_card_count = 0
+
+    @lru_cache(300)
+    def calc_for_card(card_nr: int):
+        print(".")
+        nonlocal all_card_count
+        card_res = calc_card_res(card_nr)
+        int_res = card_res
+        print(f"Card {card_nr} has {card_res} results.")
+        for i in list(range(0, card_res)):
+            target_card_nr = card_nr + i + 1
+            # print(f"Adding {target_card_nr}")
+            int_res += calc_for_card(target_card_nr)
+
+        return int_res
+
+    tot = 0
+    for card_nr in all_cards_dict.keys():
+        tot += calc_for_card(card_nr)
+
+    pts_2 = tot + len(all_cards)
+
+    print("Part 1")
+    print(pts_1)
+    print("Part 2")
+    print(pts_2)
 
 
 if __name__ == "__main__":
