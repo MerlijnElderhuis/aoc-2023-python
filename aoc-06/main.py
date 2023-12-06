23613c2c871d84b87f4303d53e5fe12547e049b6
Merlijn <merlijn@brainial.com>
2023-12-06 16:30:03 +0100

    day 6 p1 done

--- aoc-06/main.py
+++ aoc-06/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def read_input_lines(input_file: str) -> list[str]:
@@ -9,6 +10,24 @@ def read_input_lines(input_file: str) -> list[str]:
         lines = f.read().splitlines()
 
     return lines
+
+
+def parse_input(lines):
+    lines_numbers = [line.split(":")[-1] for line in lines]
+
+    lines_numbers = [re.sub(" +", " ", line.strip()) for line in lines_numbers]
+    lines_numbers = [line.split(" ") for line in lines_numbers]
+
+    games = list(map(list, zip(*lines_numbers)))
+    games = [[int(record) for record in game] for game in games]
+
+    return games
+
+
+def calc_distance(time_tot, time_hold):
+    time_left = time_tot - time_hold
+    dist = time_left * time_hold
+    return dist
 
 
 def main():
@@ -22,6 +41,26 @@ def main():
     for line in lines:
         print(line)
     print("=" * 80)
+
+    games = parse_input(lines)
+    print(games)
+    print("=" * 80)
+
+    winners_count = []
+    for game in games:
+        time = game[0]
+        record_distance = game[1]
+
+        winners = []
+        for hold_time in range(time + 1):
+            if calc_distance(time, hold_time) > record_distance:
+                winners.append(hold_time)
+        winners_count.append(len(winners))
+
+    tot = 1
+    for winner_count in winners_count:
+        tot = tot * winner_count
+    print(tot)
 
 
 if __name__ == "__main__":
