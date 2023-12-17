6e665bfc7503287a1e598456b5b034dcfcd76278
Merlijn <merlijn@brainial.com>
2023-12-17 15:04:31 +0100

    template folders

---
+++ aoc-17/main.py
@@ -0,0 +1,28 @@
+import os
+
+
+def read_input_lines(input_file: str) -> list[str]:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir = dir_path.split("/")[-1]
+    print(f"Starting {dir}")
+    with open(f"{dir}/{input_file}", "r") as f:
+        lines = f.read().splitlines()
+
+    return lines
+
+
+def main():
+    TEST = True
+    if TEST:
+        file_name = "test_input.txt"
+    else:
+        file_name = "input.txt"
+
+    lines = read_input_lines(file_name)
+    for line in lines:
+        print(line)
+    print("=" * 80)
+
+
+if __name__ == "__main__":
+    main()
